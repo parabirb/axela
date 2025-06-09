061f7314fb76c15c8d7d63379fab3a7fddf807f5
birb <parabirb@protonmail.ch>
2025-06-09 05:27:13 -0500

    fix prettier formatting

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
-import {db} from './db.js';
-import {channels} from './schema.js';
+import { db } from "./db.js";
+import { channels } from "./schema.js";
 
 await db.insert(channels).values({
-	name: '#raisedtailscafe',
+    name: "#raisedtailscafe",
 });
